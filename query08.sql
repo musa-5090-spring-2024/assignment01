9b2b5c2c92fb3f80b16627bd4dad2e105ebd62bc
Mjumbe Poe <mjumbewu@gmail.com>
2024-01-31 20:13:25 -0500

    Correct the comment in query 8

--- query08.sql
+++ query08.sql
@@ -2,7 +2,7 @@
     Give the five most popular starting stations across all years between 7am
     and 9:59am.
 
-    Your result should have 5 records with four columns, one for the station id
+    Your result should have 5 records with three columns, one for the station id
     (named `station_id`), one for the point geography of the station (named
     `station_geog`), and one for the number of trips that started at that
     station (named `num_trips`).
