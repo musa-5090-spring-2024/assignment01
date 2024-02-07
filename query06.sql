b40a239608d1bbcdbfe24d1a6b2271153ab21911
nlebovits <nissim.lebovits@vanderbilt.edu>
2024-02-07 11:45:45 -0500

    submmit queries 1 thru 7

--- query06.sql
+++ query06.sql
@@ -7,3 +7,22 @@
 */
 
 -- Enter your SQL query here
+SELECT
+    CAST(2021 AS TEXT) AS trip_year,
+    CAST(3 AS TEXT) AS trip_quarter,
+    COUNT(*) AS num_trips
+FROM
+    indego.trips_2021_q3
+WHERE
+    EXTRACT(EPOCH FROM (end_time - start_time)) / 60 < 10
+
+UNION ALL
+
+SELECT
+    CAST(2022 AS TEXT) AS trip_year,
+    CAST(3 AS TEXT) AS trip_quarter,
+    COUNT(*) AS num_trips
+FROM
+    indego.trips_2022_q3
+WHERE
+    EXTRACT(EPOCH FROM (end_time - start_time)) / 60 < 10;
--- query07.sql
+++ query07.sql
@@ -6,6 +6,34 @@
 */
 
 -- Enter your SQL query here
+SELECT
+    CAST(EXTRACT(YEAR FROM start_time) AS TEXT) AS trip_year,
+    CAST(EXTRACT(QUARTER FROM start_time) AS TEXT) AS trip_quarter,
+    COUNT(*) AS num_trips
+FROM
+    (
+        SELECT start_time, end_time FROM indego.trips_2021_q3
+        UNION ALL
+        SELECT start_time, end_time FROM indego.trips_2022_q3
+    ) AS combined_trips
+WHERE
+    CAST(start_time AS DATE) <> CAST(end_time AS DATE)
+GROUP BY trip_year, trip_quarter
+ORDER BY trip_year, trip_quarter;
+
+    CAST(EXTRACT(YEAR FROM start_time) AS TEXT) AS trip_year,
+    CAST(EXTRACT(QUARTER FROM start_time) as text) AS trip_quarter,
+    COUNT(*) AS num_trips
+FROM
+    (
+        SELECT start_time, end_time FROM indego.trips_2021_q3
+        UNION ALL
+        SELECT start_time, end_time FROM indego.trips_2022_q3
+    ) AS combined_trips
+WHERE
+    CAST(start_time AS DATE) <> CAST(end_time AS DATE)
+GROUP BY trip_year, trip_quarter
+ORDER BY trip_year, trip_quarter;
 
 
 
