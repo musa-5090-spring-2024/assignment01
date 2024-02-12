0edcddf6215c6f013e64cef4b7ce0508d4b08a8f
Van Archer <wx7@upenn.edu>
2024-02-12 17:15:14 -0500

    Update query06.sql

--- query06.sql
+++ query06.sql
@@ -7,3 +7,10 @@
 */
 
 -- Enter your SQL query here
+SELECT 2021 AS trip_year, 'Q3' AS trip_quarter, COUNT(*) AS num_trips
+FROM indego.trips_2021_q3
+WHERE duration < 10
+UNION ALL
+SELECT 2022 AS trip_year, 'Q3' AS trip_quarter, COUNT(*) AS num_trips
+FROM indego.trips_2022_q3
+WHERE duration < 10;
