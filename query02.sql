ba922d7d1ea7e2eb7e9a2e31b1b759bf0ed994d0
Van Archer <wx7@upenn.edu>
2024-02-12 17:01:14 -0500

    Update query02.sql

--- query02.sql
+++ query02.sql
@@ -10,8 +10,12 @@
 */
 
 -- Enter your SQL query here
-
-
+SELECT (ROUND(((trips_2022 - trips_2021)::NUMERIC / trips_2021) * 100, 2)::text || '%') AS perc_change
+FROM
+  (SELECT COUNT(*) AS trips_2021
+   FROM indego.trips_2021_q3),
+  (SELECT COUNT(*) AS trips_2022
+   FROM indego.trips_2022_q3);
 
 /*
     If you want to get fancier here, you can cast the result to a string and
