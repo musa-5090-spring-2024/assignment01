25405a3487cd1e5751a087dc5383be284a5bc08a
Van Archer <wx7@upenn.edu>
2024-02-13 17:04:54 -0500

    Update query12.sql

--- query12.sql
+++ query12.sql
@@ -6,3 +6,17 @@
 */
 
 -- Enter your SQL query here
+select count(*) as num_stations
+from (
+    select
+        st_distance(
+                st_transform(geog::geometry, 32129),
+                st_transform(
+                    st_setsrid(
+                        st_makepoint(-75.192584,39.952415), 4326),
+                    32129
+                    )
+                ) / 1000 as avg_distance_km
+    from indego.station_statuses
+) as subquery
+where avg_distance_km <= 1;
