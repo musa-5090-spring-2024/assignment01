24b185abbbb90f5e1d39bcd8b2634155ce4f8694
Van Archer <wx7@upenn.edu>
2024-02-13 16:52:12 -0500

    Update query08.sql

--- query08.sql
+++ query08.sql
@@ -9,7 +9,25 @@
 */
 
 -- Enter your SQL query here
-
+SELECT
+  start_station AS station_id,
+  st_setsrid(
+    ST_MakePoint(start_lon, start_lat), 4326) 
+    AS station_geog,
+  COUNT(*) AS num_trips
+FROM
+     (SELECT * FROM indego.trips_2021_q3
+      UNION ALL
+      SELECT * FROM indego.trips_2022_q3) 
+      AS combined_trips
+WHERE
+  EXTRACT(HOUR FROM start_time) >= 7 AND
+  EXTRACT(HOUR FROM start_time) < 10
+GROUP BY
+  start_station, start_lon, start_lat
+ORDER BY
+  num_trips DESC
+LIMIT 5;
 
 /*
     Hint: Use the `EXTRACT` function to get the hour of the day from the
