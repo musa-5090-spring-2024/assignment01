5b75b63c986bba9dcd7aef81bb2463e69e680208
Van Archer <wx7@upenn.edu>
2024-02-13 17:05:31 -0500

    Update query14.sql

--- query14.sql
+++ query14.sql
@@ -7,3 +7,18 @@
 */
 
 -- Enter your SQL query here
+select
+    id as station_id,
+    name as station_name,
+    round(
+            st_distance(
+                st_transform(geog::geometry, 32129),
+                st_transform(
+                    st_setsrid(
+                        st_makepoint(-75.192584,39.952415), 4326),
+                    32129
+                    )
+                ) / 50) * 50 as distance
+from indego.station_statuses
+order by distance asc
+limit 1;
