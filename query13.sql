cb65bfd6705b8fd06b2314466378e1f4a3178d8f
Trevor Kapuvari <143422872+TrevorKap@users.noreply.github.com>
2024-02-14 16:45:33 -0500

    Update query13.sql

--- query13.sql
+++ query13.sql
@@ -6,4 +6,12 @@
     Meyerson Hall, rounded to the nearest 50 meters.
 */
 
--- Enter your SQL query here
+
+SELECT
+    id AS station_id,
+    name AS station_name,
+    ROUND(ST_Distance(geog, ST_SetSRID(ST_MakePoint(-75.192584, 39.952415), 4326)) / 50) * 50 AS distance
+FROM
+    indego.station_statuses
+ORDER BY distance DESC
+LIMIT 1;
