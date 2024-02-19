770aa6fda42aad7adc43aec330f3ed8347db86c1
jingyili219 <143425355+jingyili219@users.noreply.github.com>
2024-02-19 17:21:53 -0500

    Update query06.sql

--- query06.sql
+++ query06.sql
@@ -7,3 +7,12 @@
 */
 
 -- Enter your SQL query here
+SELECT '2021' AS trip_year, 'Q3' AS trip_quarter, COUNT(*) AS num_trips
+FROM trips_2021_q3
+WHERE duration::numeric < 10
+UNION 
+SELECT '2022' AS trip_year, 'Q3' AS trip_quarter, COUNT(*) AS num_trips
+FROM trips_2022_q3
+WHERE duration::numeric < 10
+/* result: 2021 has 124528 trips less than 10 mintues, 2022 has 137372 trips less than 10 mintues.
+*/
