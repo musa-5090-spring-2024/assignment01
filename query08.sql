2ecaa214858a36ea9c94ecda49b5554e7b14aa08
jingyili219 <143425355+jingyili219@users.noreply.github.com>
2024-02-19 17:40:38 -0500

    Update query08.sql

--- query08.sql
+++ query08.sql
@@ -9,7 +9,21 @@
 */
 
 -- Enter your SQL query here
-
+SELECT start_station AS station_id, COUNT(start_station) AS num_trips, st_setsrid(st_makepoint(start_lon::FLOAT, start_lat::FLOAT), 4326) AS station_geog
+FROM (
+    (SELECT start_station, start_lon, start_lat
+     FROM trips_2021_q3
+     WHERE EXTRACT(HOUR FROM start_time::timestamp) >= 7 AND EXTRACT(HOUR FROM start_time::timestamp) < 10)
+    UNION 
+    (SELECT start_station, start_lon, start_lat
+     FROM trips_2022_q3
+     WHERE EXTRACT(HOUR FROM start_time::timestamp) >= 7 AND EXTRACT(HOUR FROM start_time::timestamp) < 10)
+) AS subquery
+GROUP BY start_station, start_lon, start_lat
+ORDER BY num_trips DESC
+LIMIT 5;
+/* result: "3197", "3115", "3208", "3099","3111"
+*/
 
 /*
     Hint: Use the `EXTRACT` function to get the hour of the day from the
