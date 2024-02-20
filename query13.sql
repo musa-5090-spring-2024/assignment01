05fc7d16162537263dabda68fed87ff93f110978
jingyili219 <143425355+jingyili219@users.noreply.github.com>
2024-02-19 19:34:01 -0500

    Update query13.sql

--- query13.sql
+++ query13.sql
@@ -7,3 +7,12 @@
 */
 
 -- Enter your SQL query here
+SELECT id,geom_32129 AS station_geog,
+ROUND(ST_Distance(geom_32129, ST_Transform
+    (ST_SetSRID(ST_Makepoint(-75.192584, 39.952415), 4326),32129)
+    )/50)*50 AS distance
+FROM station_status
+Order BY distance DESC
+LIMIT 1;
+/* result: 3323	"0101000020817D0000A5C9DFC449E1284154FAAB4CA094F340"	8900
+*/
