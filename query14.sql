c4b5880eaa7b3c291fca995fe4139972d5b41223
jonathan-manurung <143425047+jonathan-manurung@users.noreply.github.com>
2024-02-22 14:41:10 -0500

    Update query14.sql

--- query14.sql
+++ query14.sql
@@ -7,3 +7,13 @@
 */
 
 -- Enter your SQL query here
+
+SELECT
+  id AS station_id,
+  name AS station_name,
+  ROUND(ST_Distance(ST_SetSRID(ST_MakePoint(coordinates[1], coordinates[2]), 4326)::geography, ST_MakePoint(-75.192584, 39.952415)::geography)/50)*50 AS distance
+  FROM 
+  indego.indego_station_statuses
+ORDER BY
+  distance ASC
+LIMIT 1;
