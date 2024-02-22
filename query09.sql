43a61b4d9469d8e7a8136603a8b66049d61af567
jonathan-manurung <143425047+jonathan-manurung@users.noreply.github.com>
2024-02-22 14:39:06 -0500

    Update query09.sql

--- query09.sql
+++ query09.sql
@@ -7,3 +7,15 @@
 */
 
 -- Enter your SQL query here
+
+SELECT
+  passholder_type,
+  COUNT(*) AS num_trips
+FROM (
+	SELECT passholder_type FROM indego.trips_2021_q3
+	UNION ALL
+	SELECT passholder_type FROM indego.trips_2022_q3
+	)
+	AS combined_table
+GROUP BY
+  passholder_type;
